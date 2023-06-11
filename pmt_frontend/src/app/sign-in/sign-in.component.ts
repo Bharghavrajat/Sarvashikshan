d3797572a419fc1c9a2f4ba949ad861a107de638
mrcoder146 <letscode146@gmail.com>
2023-06-11 15:45:54 +0530

    add signin and signup

--- pmt_frontend/src/app/sign-in/sign-in.component.ts
+++ pmt_frontend/src/app/sign-in/sign-in.component.ts
@@ -1,44 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+// import { ReCaptchaV3Service } from 'ng-recaptcha';
 
-@Component({
+
+@Component({ 
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'] 
 })
 export class SignInComponent implements OnInit {
   signInForm: FormGroup;
   hide: boolean = false;
-  
-  user={
-    email:"",
-    password:""
+  // // captchaResponse: string;
+
+
+  user = {
+    email: "", 
+    password: ""
   }
 
-  constructor(private router:Router, private _snackBar: MatSnackBar) {}
+  constructor( 
+    private router: Router,
+    private _snackBar: MatSnackBar,
+    private http: HttpClient
+    // private recaptchaV3Service: ReCaptchaV3Service
+  ) { }
 
   ngOnInit(): void {
-      this.signInForm = new FormGroup(
-        {
-          email: new FormControl('',[Validators.required,Validators.email]),
-          password: new FormControl('',[Validators.required,Validators.minLength(6)])
-        }
-      )
+    this.signInForm = new FormGroup(
+      {
+        email: new FormControl(''),
+        password: new FormControl('')
+      }
+    );
   }
-
-  message:string = "Registration successful!";
-  action:string = "close";
 
   onSignIn() {
-    if(this.user.email === "admin@gmail.com" && this.user.password === "12345678") {
+    const loginData = {
+      emailId: this.user.email,
+      password: this.user.password
+    };
 
-        this._snackBar.open(this.message,this.action);
-      
-        this.router.navigate(['/select-project']);
-    }
+    this.http.post('http://localhost:8080/api/v1/login', loginData, { responseType: 'text' }).subscribe(
+      (response:any) => {
+        // Login successful, handle the response
+        this._snackBar.open(response, 'Close');
+
+        if(response === "Login successful"){
+          this.router.navigate(['/select-project']);
+        }
+        
+      },
+      (error) => {
+        // Login failed, handle the error
+        console.error(error);
+        this._snackBar.open(error.error, 'Close');
+      }
+    );
   }
-
 }
