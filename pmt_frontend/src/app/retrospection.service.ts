e11405964a830e1d3d339ae5fd8fd3dc80527f29
Your Name <you@example.com>
2023-06-15 09:24:30 +0530

    retrospection

--- pmt_frontend/src/app/retrospection.service.ts
+++ pmt_frontend/src/app/retrospection.service.ts
@@ -1,24 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Retrospection } from './classes/retrospection';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError,throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RetrospectionService {
 
-  private apiUrl ="http://localhost:7825/api/v1/retrospection";
+  private apiUrl = "http://localhost:59031/api/v1/retrospection";
 
   constructor(private http: HttpClient) { }
-  
-  getretrospection(): Observable<Retrospection[]>{
+
+  getretrospection(): Observable<Retrospection[]> {
     return this.http.get<Retrospection[]>(`${this.apiUrl}`);
   }
 
   openretrospection(retrospection: any): Observable<any> {
     return this.http.post<any>(this.apiUrl, retrospection);
   }
-
-
-  }
+}
